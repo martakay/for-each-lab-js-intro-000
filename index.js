6751d7b6c2c57e199e3f8f55948ce3975d966291
Marta Kandova <kmartaaaa1989@gmail.com>
2019-02-04 16:21:30 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,14 @@
+function iterativeLog( array ) {
+  array.forEach(( element, index, array ) => {
+    console.log(`${index}: ${element}`);
+  } );
+}
+
+function iterate(callback) {
+  var colors = ["purple", "blue", "green", "red"]
+
+  colors.forEach( color =>
+  {
+    
+  } )
+}
